c7acc9cb8ece8b89155a4c6922af00c436a0d094
silvathebest <silverchat228@gmail.com>
2020-01-18 13:39:53 +0300

    2

--- JavaProjectSeaPlane1/src/EngineDoubleOval.java
+++ JavaProjectSeaPlane1/src/EngineDoubleOval.java
@@ -1,15 +1,9 @@
 import java.awt.Color;
 import java.awt.Graphics;
-public class Engine implements IEngine{
 
-	public  void DrawEngine(Graphics g, int x,int y) {
-		g.setColor(Color.DARK_GRAY);
-		g.fillOval(  x + 50, y + 20, 15, 7);
-		g.setColor(Color.BLACK);
-		g.fillOval(  x + 60, y + 20, 5, 7);
-	}
-	@Override
-	public void draw(EngineCount count, Graphics g, int x, int y, Color maiColor) {
+public class EngineDoubleOval implements IEngine{
+	public  void draw(EngineCount count, Graphics g,  int x ,int y,Color mainColor)
+	{
 		DrawEngine(g,x-50,y); 
 		DrawEngine(g,x-55,y+5); 
 		DrawEngine(g,x-45,y-5); 		
@@ -22,6 +16,11 @@ public class Engine implements IEngine{
 		if(count == EngineCount.Six) {		  
 			DrawEngine(g,x-10,y+10);          
 		}
-		
+	}
+	public  void DrawEngine(Graphics g, int x,int y) {
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(  x + 50, y + 20, 15, 7);
+		g.setColor(Color.BLACK);
+		g.fillOval(  x + 60, y + 20, 5, 7);
 	}
 }
